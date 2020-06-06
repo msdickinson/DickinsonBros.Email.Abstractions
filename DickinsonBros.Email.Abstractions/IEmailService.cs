009fdaf3d9922d15fc5a82a414143e37aa16d7e1
Mark Dickinson <marksamdickinson@gmail.com>
2020-06-06 15:29:44 -0700

    Add project files.

---
+++ DickinsonBros.Email.Abstractions/IEmailService.cs
@@ -0,0 +1,10 @@
+﻿using MimeKit;
+using System.Threading.Tasks;
+
+namespace DickinsonBros.Email.Abstractions
+{
+    public interface IEmailService
+    {
+        Task SendAsync(MimeMessage message);
+    }
+}
