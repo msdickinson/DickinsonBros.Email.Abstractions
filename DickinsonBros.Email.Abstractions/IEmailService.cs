3878d3b5265118671504f540e0c5befd39735ea3
Mark Dickinson <marksamdickinson@gmail.com>
2020-10-08 03:58:18 -0700

    Add ValidateEmailDomain and IsValidEmailFormat (#6)

--- DickinsonBros.Email.Abstractions/IEmailService.cs
+++ DickinsonBros.Email.Abstractions/IEmailService.cs
@@ -6,5 +6,7 @@ namespace DickinsonBros.Email.Abstractions
     public interface IEmailService
     {
         Task SendAsync(MimeMessage message);
+        Task<bool> ValidateEmailDomain(string emailDomain);
+        bool IsValidEmailFormat(string email);
     }
 }
